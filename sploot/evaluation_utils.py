b80f67e810b5f84dbae855069e4094be9a00cc03
eric <ekchea@gmail.com>
2018-05-19 11:16:20 -0400

    Add docstring to decision boundary plot.

--- sploot/evaluation_utils.py
+++ sploot/evaluation_utils.py
@@ -1,4 +1,24 @@
+"""Utilities to diagnose a model.
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
 def plot_decision_boundary(model, X, y):
+    """Visualize multi-dimensions in 2D, enabling a visual inspection of how
+    overfit a model might be.
+
+    Parameters
+    ----------
+    model: function, a function that can take a feature array and target vector
+        as inputs. i.e. LogisticRegression.predict from sklearn is acceptable.
+    X: array, (# of samples, # of features) array.
+    y: vector, 1D array-like object (# of samples, 1).
+
+    Returns
+    -------
+    None, plots the decision boundary.
+    """
     # Set min and max values and give it some padding
     x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
     y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
